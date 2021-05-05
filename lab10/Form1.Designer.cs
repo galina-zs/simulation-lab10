e377d824f7ef1d11af3f7c3cd483c048e9d908c4
galina-zs <zab.galina.24@gmail.com>
2021-05-05 13:54:40 +0300

    Task is done

---
+++ lab10/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace lab10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textLabel = new System.Windows.Forms.Label();
+            this.answerLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textLabel
+            // 
+            this.textLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textLabel.Location = new System.Drawing.Point(12, 26);
+            this.textLabel.Name = "textLabel";
+            this.textLabel.Size = new System.Drawing.Size(216, 51);
+            this.textLabel.TabIndex = 0;
+            this.textLabel.Text = "Выпавшее число на шестигранном кубике:";
+            // 
+            // answerLabel
+            // 
+            this.answerLabel.AutoSize = true;
+            this.answerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.answerLabel.Location = new System.Drawing.Point(244, 40);
+            this.answerLabel.Name = "answerLabel";
+            this.answerLabel.Size = new System.Drawing.Size(0, 37);
+            this.answerLabel.TabIndex = 1;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(16, 93);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Получить";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 147);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.answerLabel);
+            this.Controls.Add(this.textLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label textLabel;
+        private System.Windows.Forms.Label answerLabel;
+        private System.Windows.Forms.Button startButton;
+    }
+}
+
---
+++ lab10/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab10
+{
+    public partial class Form1 : Form
+    {
+
+        //Пусть центр тяжести игрального кубика смещён на 6
+        //Тогда пусть ряд распределения:
+        //
+        //    x | 1     2       3       4       5       6
+        //    p | 0.05  0.1625  0.1625  0.1625  0.1625  0.3
+
+        readonly double[] probabilitis = new double[6] { 0.05, 0.1625, 0.1625, 0.1625, 0.1625, 0.3 };
+
+        double U = 1;
+        const int p = 5087; // большое простое число
+        int M = 2900;       // M = p - 3^n. Берем n = 7, потому что 3^8 > p
+        double R;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            Generator();
+            double tempR = R;
+            for (int i = 0; i < probabilitis.Length; i++)
+            {
+                tempR -= probabilitis[i];
+                if (tempR <= 0)
+                {
+                    answerLabel.Text = (i + 1).ToString();
+                    break;
+                }
+            }
+        }
+
+        private void Generator() // метод вычетов. Модификация Коробова
+        {
+            R = U / p;
+            U = (U * M) % p;
+        }
+    }
+}
